609a6748aa2e787f4950bce8136b583a317eb188
Korea-subinpark <subinpark.re@gmail.com>
2020-06-20 00:06:45 +0900

    Posts 등록까지 구현

---
+++ src/main/java/com/subin/blog/springboot/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.subin.blog.springboot.service.posts;
+
+import com.subin.blog.springboot.domain.posts.PostsRepository;
+import com.subin.blog.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
---
+++ src/main/java/com/subin/blog/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,13 @@
+package com.subin.blog.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+
+}
